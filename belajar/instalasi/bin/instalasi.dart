1d03024b52a134ad48202796befe39a45376ad39
keepCoding <143472198+keepCoding01@users.noreply.github.com>
2025-08-15 23:20:30 +0700

    menambahkan file

---
+++ belajar/instalasi/bin/instalasi.dart
@@ -0,0 +1,5 @@
+import 'package:instalasi/instalasi.dart' as instalasi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${instalasi.calculate()}!');
+}
---
+++ belajar/instalasi/test/instalasi_test.dart
@@ -0,0 +1,8 @@
+import 'package:instalasi/instalasi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
